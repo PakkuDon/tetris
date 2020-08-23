78d8faf5a842d18cc014d514795e95a09a91d97a
PakkuDon <pakku.pd@gmail.com>
2020-08-23 23:57:23 +1000

    Implement method to draw grid

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const game = new Game()
+const gridCanvas = document.querySelector('canvas')
+const gridGraphicsContext = gridCanvas.getContext('2d')
+
+const drawGrid = (grid, graphicsContext) => {
+  const canvasWidth = graphicsContext.canvas.width
+  const canvasHeight = graphicsContext.canvas.height
+  const cellWidth = canvasWidth / grid[0].length
+  const cellHeight = canvasHeight / grid.length
+
+  graphicsContext.clearRect(0, 0, canvasWidth, canvasHeight)
+
+  for (var y = 0; y < grid.length; y++) {
+    for (var x = 0; x < grid[y].length; x++) {
+      graphicsContext.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight)
+    }
+  }
+}
+
+drawGrid(game.grid, gridGraphicsContext)
